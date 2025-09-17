3698167eb70c4b06394fa88e0bee5e839dabcd8b
cordy <mglgain@gmail.com>
2025-09-17 15:56:28 +0800

    refactor: modify CSS module of banner, Static, and Placeholder

--- src/app/components/landpage/Static/index.tsx
+++ src/app/components/landpage/Static/index.tsx
@@ -1,11 +1,15 @@
 import { Static__css } from "@/app/config/css/landpage";
 
-
 export default function Static() {
     return(
         <section>
             <div className={Static__css.static__container}>
+                <div className={Static__css.static__left_side}>
+                    
+                </div>
+                <div className={Static__css.static__right_side}>
 
+                </div>
             </div>
         </section>
     );
