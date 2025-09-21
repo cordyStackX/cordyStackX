20d5c01eab6c410b9f5da8d44fd6015f2722b9cd
cordy <mglgain@gmail.com>
2025-09-21 13:22:25 +0800

    feat: add new packages next-sitemap for recommendations

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cordy-stack-x.vercel.app', // your production URL
+  generateRobotsTxt: true, // automatically creates robots.txt
+};
