3d41e48502eb1cb15a6740c376a763c649df84d3
cordy <mglgain@gmail.com>
2025-09-21 13:29:46 +0800

    refactor: modify next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://cordy-stack-x.vercel.app', // your production URL
-  generateRobotsTxt: true, // automatically creates robots.txt
+  siteUrl: 'https://cordy-stack-x.vercel.app', // your live domain
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
 };
