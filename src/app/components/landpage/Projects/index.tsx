f4430a4fbfdf82116e841d19f66e307ecc6ccf15
cordy <mglgain@gmail.com>
2025-09-24 02:33:23 +0800

    feat and refactor: add new UI style and improves effects of services components

--- src/app/components/landpage/Projects/index.tsx
+++ src/app/components/landpage/Projects/index.tsx
@@ -1,7 +1,10 @@
 import { Projects__css } from "@/app/config/css/landpage";
 import image__src from "@/app/config/conf/images.json";
+import project__src from "@/app/config/conf/project__links.json";
 import Image from "next/image";
 import { useInView } from "@/app/utilities";
+import Link from "next/link";
+import { UseRef } from "@/app/modules";
 
 export default function Projects() {
     const treshold = 0.8;
@@ -9,9 +12,29 @@ export default function Projects() {
     const { ref: grow1, isInView: growIsInView1 } = useInView<HTMLDivElement>(false, treshold);
     const { ref: grow2, isInView: growIsInView2 } = useInView<HTMLDivElement>(false, treshold);
     const { ref: grow3, isInView: growIsInView3 } = useInView<HTMLDivElement>(false, treshold);
-    const { ref: grow4, isInView: growIsInView4 } = useInView<HTMLDivElement>(false, treshold);
-    const { ref: grow5, isInView: growIsInView5 } = useInView<HTMLDivElement>(false, treshold);
-    const { ref: grow6, isInView: growIsInView6 } = useInView<HTMLDivElement>(false, treshold);
+    // const { ref: grow4, isInView: growIsInView4 } = useInView<HTMLDivElement>(false, treshold);
+    // const { ref: grow5, isInView: growIsInView5 } = useInView<HTMLDivElement>(false, treshold);
+    // const { ref: grow6, isInView: growIsInView6 } = useInView<HTMLDivElement>(false, treshold);
+
+    const scrollRef = UseRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+        if (scrollRef.current) {
+        scrollRef.current.scrollBy({
+            left: -300,
+            behavior: "smooth",
+        });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollRef.current) {
+        scrollRef.current.scrollBy({
+            left: 300, 
+            behavior: "smooth",
+        });
+        }
+    };
 
     return(
         <section id="Projects" className={Projects__css.projects__sections}>
@@ -20,23 +43,47 @@ export default function Projects() {
                     <h2>Projects</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia vel, error sequi similique mollitia</p>
                 </div>
-                <div className={Projects__css.projects__status}>
+                <div className={Projects__css.projects__status} ref={scrollRef}>
                     <div ref={grow1} className={`${Projects__css.projects__stat} ${growIsInView1 ? Projects__css.projects__stat_grow : ''}`}>
-                        <div className={Projects__css.projects__stat_image_title}>
+                        <div className={Projects__css.projects__stat_imageContain}>
                             <div className={Projects__css.projects__stat_image}>
                                 <Image
-                                src={image__src.services.image__1.imageSource}
+                                src={image__src.logo.image__1}
                                 alt="Sample"
                                 title="Samples"
-                                width={200}
-                                height={200}
+                                width={170}
+                                height={170}
                                 unoptimized
-                                className={Projects__css.projects__stat_image_line}
                                 />
                             </div>
+                            <div className={Projects__css.projects__stat_image__cover}>
+                               <Image
+                                src={image__src.cover__photo.image__1}
+                                alt="Cover"
+                                title="Cover"
+                                fill
+                                unoptimized
+                                />
+                            </div>
+
+                        </div>
+                        <div className={Projects__css.projects__stat_title_buttons}>
+                            <h2>Cordy Connects</h2>
+                            <span>
+                                <button> <Link href={project__src.cordy__connect.visit}>Visit</Link> </button>
+                                <button> <Link href={project__src.cordy__connect.repo}>Open {'>>'}</Link> </button>
+                            </span>
+                            
                         </div>
                         <div className={Projects__css.projects__stat__info}>
-
+                            <p>lightweight and customizable wallet connector built for Web3 applications. 
+                                Inspired by RainbowKit, it streamlines wallet integration while keeping 
+                                the developer experience simple and flexible. With a focus on speed, ease of use, 
+                                and seamless UI customization, Cordy Connect empowers dApps to onboard users effortlessly.</p>
+                        </div>
+                        <div className={Projects__css.projects__stat_navigators}>
+                            <button onClick={scrollLeft}> {'<<'} Prev</button> 
+                            <button onClick={scrollRight}>Next {'>>'} </button>
                         </div>
                     </div>
                     <div ref={grow2} className={`${Projects__css.projects__stat} ${growIsInView2 ? Projects__css.projects__stat_grow : ''}`}>
@@ -45,7 +92,7 @@ export default function Projects() {
                     <div ref={grow3} className={`${Projects__css.projects__stat} ${growIsInView3 ? Projects__css.projects__stat_grow : ''}`}>
                         Card 3
                     </div>
-                    <div ref={grow4} className={`${Projects__css.projects__stat} ${growIsInView4 ? Projects__css.projects__stat_grow : ''}`}>
+                    {/* <div ref={grow4} className={`${Projects__css.projects__stat} ${growIsInView4 ? Projects__css.projects__stat_grow : ''}`}>
                         Card 4
                     </div>
                     <div ref={grow5} className={`${Projects__css.projects__stat} ${growIsInView5 ? Projects__css.projects__stat_grow : ''}`}>
@@ -53,7 +100,7 @@ export default function Projects() {
                     </div>
                     <div ref={grow6} className={`${Projects__css.projects__stat} ${growIsInView6 ? Projects__css.projects__stat_grow : ''}`}>
                         Card 6
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
