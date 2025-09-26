b4b0446e01f5cfe5c34f0e5fa62c973908980128
cordy <mglgain@gmail.com>
2025-09-27 04:42:49 +0800

    refactor: modify next config for improvements of LCP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'www.svgrepo.com',
+      'upload.wikimedia.org',
+      'wagmi.sh',
+      'icon.icepanel.io',
+      'cdn-icons-png.flaticon.com',
+      'cdn.worldvectorlogo.com',
+      'https://encrypted-tbn0.gstatic.com',   // your CDN
+      'images.unsplash.com', // if you test with Unsplash
+      'ipfs.io',       // Web3 storage
+      'raw.githubusercontent.com' // GitHub hosted images
+    ],
+    formats: ['image/avif', 'image/webp'], // ✅ faster formats
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
