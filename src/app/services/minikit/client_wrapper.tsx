41e00e4bad0bf7a6634881f2fb861c49c7a9217b
cordy <mglgain@gmail.com>
2025-10-04 03:26:37 +0800

    refactor: modify Projects width layouts

--- src/app/services/minikit/client_wrapper.tsx
+++ src/app/services/minikit/client_wrapper.tsx
@@ -4,9 +4,9 @@ import dynamic from 'next/dynamic';
 
 // Dynamically import WalletProviders with SSR disabled
 const WalletProviders = dynamic(() => import('./providers'), {
-ssr: false,
+    ssr: false,
 });
 
 export default function ClientWrapper({ children }: { children: ReactNode }) {
-return <WalletProviders>{children}</WalletProviders>;
+    return <WalletProviders>{children}</WalletProviders>;
 }
