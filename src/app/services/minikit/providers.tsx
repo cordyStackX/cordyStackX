014266936af29256aee1fe7a53b99ca210802e51
cordy <mglgain@gmail.com>
2025-10-16 06:30:38 +0800

    feat and update: Updating Cordy Minikit and Add chains customizations

--- src/app/services/minikit/providers.tsx
+++ src/app/services/minikit/providers.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from "react";
 import { ProvidersClientWrapper } from "@cordystackx/cordy_minikit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
